4a0d64659b25d2180eeddfdceec9dd249e19c2bc
Francis Vargas <francis.vargas@sansano.usm.cl>
2017-09-25 14:17:33 -0400

    Update DBconnect.js

--- Proyecto-ADSW/connection/DBconnect.js
+++ Proyecto-ADSW/connection/DBconnect.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection( {
     host     : 'localhost',
     user     : 'root',
-    password : 'Carlosduty12!',
+    password : '',
     database : 'ProyectoAD'
 });
 
